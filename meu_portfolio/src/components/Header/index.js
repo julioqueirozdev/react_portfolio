5a70abd55f80ed285f4e4c8026f5c876b93de941
Júlio Queiroz <jcqaraujo@gmail.com>
2025-02-11 16:52:50 -0400

    doc: dados e dinamismo da exibição

--- meu_portfolio/src/components/Header/index.js
+++ meu_portfolio/src/components/Header/index.js
@@ -1,26 +1,26 @@
-import './Header.css';
+import "./Header.css";
+import { profile } from "../../data/profile";
 
-export default function Header(picture){
-    return(
-        <div className="header">
-            <div className="profile-inner">
+export default function Header() {
+  return (
+    <div className="header">
+      <div className="profile-inner">
+        {profile.map(person =>{
+         return(
+          <>
           <div className="profile-text">
-            <h1 id="greetings">Muito prazer!</h1>
-            <p>
-              Meu nome é Júlio Queiroz e eu sou um desenvolvedor front-end!
-            </p>
-          </div>
-          <div className="profile-image">
-            <picture>
-              <img
-                width="500"
-                height="500"
-                src={picture}
-                alt="profile picture"
-              />
-            </picture>
-          </div>
+          <h1 id="greetings">Muito prazer!</h1>
+          <p>{person.greetings}</p>
         </div>
+        <div className="profile-image">
+          <picture>
+            <img width="500" height="500" src= './src/images/profile.jpg' alt={person.name} />
+          </picture>
         </div>
-    );
+          </>
+         ); 
+        } )} 
+      </div>
+    </div>
+  );
 }
---
+++ meu_portfolio/src/data/profile.js
@@ -0,0 +1,18 @@
+export const profile = [
+    {
+        id: 0,
+        name: "Júlio Queiroz",
+        avatar: "./profile.jpg",
+        greetings: "Meu nome é Júlio Queiroz e eu sou um desenvolvedor front-end!",
+        email:"jcqaraujo@gmail.com",
+        github:"https://github.com/julioqueirozdev",
+        linkedin: "https://www.linkedin.com/in/juliocqaraujo/",
+        about: [
+            {
+                description: "Eu transformo código em realidade no navegador!",
+                lenguages: ["HTML", "Javascript", "CSS", "Git"],
+                tools:["React", "Github", "Vs Code"]
+            }
+        ]
+    }
+]
