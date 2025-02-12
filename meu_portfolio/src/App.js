32d2dc280dd5759c148b17d0f2ba20c218bda8b6
Júlio Queiroz <jcqaraujo@gmail.com>
2025-02-12 17:48:03 -0400

    doc: refatoração do componente card e componente projectGrid

--- meu_portfolio/src/App.js
+++ meu_portfolio/src/App.js
@@ -4,15 +4,23 @@ import Header from "./components/Header";
 import Project from "./components/Project";
 import Skills from "./components/Skills";
 import './queries.css';
+import projects from './data/projects.json'
+import ProjectGrid from "./components/ProjectGrid";
 
 
 function App() {
   return (
     <div className="App">
-      <Header picture={''}/> 
+      <Header/> 
       <Skills/>
       <Project/>
-      <Cards/>
+      <ProjectGrid>
+        {projects.map(project => {
+        return(
+          <Cards key={project.id} name={project.name} description={project.description} url={project.url} github={project.github}/>
+        );
+      })}
+      </ProjectGrid>
       <Footer/>
     </div>
   );
---
+++ meu_portfolio/src/components/ProjectGrid/index.js
@@ -0,0 +1,9 @@
+import './ProjectGrid.css'
+
+export default function ProjectGrid({children}){
+    return(
+        <div className='project-grid'> 
+            {children}
+        </div>
+    );
+}
